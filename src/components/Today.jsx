e3b94c6a08418bccc2439a6e8947a6c1e11ec5c9
lucas <lucasisanderson@gmail.com>
2023-07-24 12:50:52 -0400

    Added icons and color to the Today component

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,3 +1,6 @@
+import { BsEye, BsSunrise, BsSunset, BsThermometerHalf, BsUmbrella } from "react-icons/bs";
+import { PiWindFill } from "react-icons/pi";
+
 /* eslint-disable react/prop-types */
 const Today = ({ data, units, timezone }) => {
 	const hourFormatter = Intl.DateTimeFormat(timezone.options.locale, {
@@ -13,46 +16,85 @@ const Today = ({ data, units, timezone }) => {
 		weekday: "long",
 	});
 	return (
-		<div className="mx-2 lg:text-md sm:text-md">
+		<div className="">
 			<div className="w-full items-center flex flex-row justify-between">
 				<div className="text-2xl inline-flex mr-3">Today</div>
 				<div className="text-md text-gray-300 ">{dateFormatter.format(new Date())}</div>
 			</div>
-			<div className="flex flex-row justify-between py-2 border-gray-800 border-b-[1px]">
-				<label>High/Low</label>
+			<div className="flex flex-row justify-between py-2 border-gray-800/50 border-b-[1px] ">
+				<div className="flex flex-auto items-center justify-start">
+					<div className="basis-7 mt-1">
+						<BsThermometerHalf size={20} className="" />
+					</div>
+					<div className="text-sky-300">High/Low</div>
+				</div>
 				<label className="whitespace-nowrap">
 					{Math.round(data.days[0].tempmax)}° / {Math.round(data.days[0].tempmin)}°
 				</label>
 			</div>
-			<div className="flex flex-row justify-between py-2 border-gray-800 border-b-[1px]">
-				<label>Feels like</label>
+			<div className="flex flex-row justify-between py-2 border-gray-800/50 border-b-[1px] ">
+				<div className="flex flex-auto items-center justify-start">
+					<div className="basis-7 mt-1">
+						<BsThermometerHalf size={20} className="" />
+					</div>
+					<div className="text-sky-300">Feels Like</div>
+				</div>
 				<label>
 					{Math.round(data.days[0].feelslikemax)}° / {Math.round(data.days[0].feelslikemin)}°
 				</label>
 			</div>
-			<div className="flex flex-row justify-between py-2 border-gray-800 border-b-[1px]">
-				<label>Wind</label>
+			<div className="flex flex-row justify-between py-2 border-gray-800/50 border-b-[1px] ">
+				<div className="flex flex-auto items-center justify-start">
+					<div className="basis-7 mt-1">
+						<BsUmbrella size={20} className="" />
+					</div>
+					<div className="text-sky-300">Precipitation</div>
+				</div>
+
+				<label>{data.days[0].precipprob}%</label>
+			</div>
+			<div className="flex flex-row justify-between py-2 border-gray-800/50 border-b-[1px] ">
+				<div className="flex flex-auto items-center justify-start">
+					<div className="basis-7 mt-1">
+						<PiWindFill size={20} />
+					</div>
+					<div className="text-sky-300">Wind</div>
+				</div>
 				<label>
 					{/* {data.windgusts_10m_max_best_match[0]} */}
 					{Math.round(data.days[0].windspeed)} {units.wind}
 				</label>
 			</div>
-			<div className="flex flex-row justify-between py-2 border-gray-800 border-b-[1px]">
-				<label>Sunrise/Sunset</label>
-				<label>
-					{`${hourFormatter.format(new Date(data.currentConditions.sunriseEpoch * 1000))}`} /{" "}
-					{`${hourFormatter.format(new Date(data.currentConditions.sunsetEpoch * 1000))}`}
-				</label>
-			</div>
-			<div className="flex flex-row justify-between py-2 border-gray-800 border-b-[1px]">
-				<label>Visibility</label>
+			<div className="flex flex-row justify-between py-2 border-gray-800/50 border-b-[1px] ">
+				<div className="flex flex-auto items-start justify-start">
+					<div className="basis-7 mt-1">
+						<BsEye size={17} className="" />
+					</div>
+					<div className=" text-sky-300">Visibility</div>
+				</div>
 				<label>
 					{data.currentConditions.visibility} {units.visibility}
 				</label>
 			</div>
-			<div className="flex flex-row justify-between my-2">
-				<label>Precipitation</label>
-				<label>{data.days[0].precipprob}%</label>
+			<div className="flex flex-col">
+				<div className="flex flex-row justify-between py-2 border-gray-800/50">
+					<div className="flex flex-auto items-center justify-start">
+						<div className="basis-7 mt-1">
+							<BsSunrise size={20} className="" color="orange" />
+						</div>
+						<div className="text-sky-300">Sunrise</div>
+					</div>
+					<div className="">{`${hourFormatter.format(new Date(data.currentConditions.sunriseEpoch * 1000))}`} </div>
+				</div>
+				<div className="flex flex-row justify-between border-gray-800/50">
+					<div className="flex flex-auto items-center justify-start">
+						<div className="basis-7 mt-1">
+							<BsSunrise size={20} className="" color="orange" />
+						</div>
+						<div className="text-sky-300">Sunset</div>
+					</div>
+					<div className="">{`${hourFormatter.format(new Date(data.currentConditions.sunsetEpoch * 1000))}`} </div>
+				</div>
 			</div>
 		</div>
 	);
