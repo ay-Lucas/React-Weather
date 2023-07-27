d220f701480a0c1fcba3ba27ff881664689d85fe
lucas <lucasisanderson@gmail.com>
2023-07-27 19:16:36 -0400

    corrected scroll tracking and color changing in horizontal scroll

--- src/components/HourlyInterface.jsx
+++ src/components/HourlyInterface.jsx
@@ -14,10 +14,10 @@ export const HourlyInterface = ({ children, hours, data, startingIndex, timezone
 		const time = locationHourFormatter.format(date);
 		return time;
 	};
-	const [color, setColor] = useState(cyan[200]);
+	const [color, setColor] = useState(colors[0]);
 	const [date, setDate] = useState(getDateString(data.days[0].datetimeEpoch));
 	const pixelsInOneScroll = 1000;
-	const scrollsInDay = 3.43;
+	const scrollsInDay = 3;
 	const pixelsInDay = pixelsInOneScroll * scrollsInDay;
 	const tilesInView = 7;
 
